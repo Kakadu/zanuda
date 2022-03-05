f0f38f697b44f8464c8b2b9b7d624ad78a80137e
Kakadu <Kakadu@pm.me>
2022-03-05 18:46:57 +0300

    Preparing to work on unified diff
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ src/unidiff/parser.ml
@@ -0,0 +1,19 @@
+(*
+commit:
+  https://github.com/<owner>/<repo>/commit/<commit>.diff
+  wget https://github.com/Kakadu/mylinter/commit/e4d35c2.diff
+
+pull:
+   wget https://github.com/Kakadu/mylinter/commit/e4d35c2.diff
+*)
+
+(*
+doc: https://www.artima.com/weblogs/viewpost.jsp?thread=164293
+https://github.com/reviewdog/reviewdog/blob/master/diff/parse.go
+https://www.gnu.org/software/diffutils/manual/html_node/Detailed-Unified.html
+*)
+open Angstrom
+
+let title = fail "not implemented"
+let hunk = fail "not implemented"
+let main = title *> many hunk
