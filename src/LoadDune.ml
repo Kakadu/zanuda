24dcb7b9a1f0240f4bab26a4ea3e736ef093e702
Kakadu <Kakadu@pm.me>
2023-05-24 12:35:29 +0300

    chore: Add try/with to defend from parsing error
    It usually happens when analysis of parsetree meets some camlp5 syntax
    extensions.
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- src/LoadDune.ml
+++ src/LoadDune.ml
@@ -31,8 +31,13 @@ let analyze_dir ~untyped:analyze_untyped ~cmt:analyze_cmt ~cmti:analyze_cmti pat
   in
   let on_module _ m =
     (* we analyze syntax tree without expanding syntax extensions *)
-    Option.iter m.impl ~f:analyze_untyped;
-    Option.iter m.intf ~f:analyze_untyped;
+    let try_untyped filename =
+      try analyze_untyped filename with
+      | Syntaxerr.Error _e ->
+        Format.eprintf "Syntaxerr.Error in analysis of '%s'. Skipped.\n%!" filename
+    in
+    Option.iter m.impl ~f:try_untyped;
+    Option.iter m.intf ~f:try_untyped;
     (* Now analyze Typedtree extracted from cmt[i] *)
     let on_cmti source_file (_cmi_info, cmt_info) =
       Option.iter cmt_info ~f:(fun cmt ->
@@ -47,41 +52,40 @@ let analyze_dir ~untyped:analyze_untyped ~cmt:analyze_cmt ~cmti:analyze_cmti pat
     in
     List.iter
       [ m.impl, m.cmt; m.intf, m.cmti ]
-      ~f:
-        (function
-         | None, None -> ()
-         | Some filename, None ->
-           Caml.Format.printf "Found ml[i] file '%s' without cmt[i] file\n" filename
-         | None, Some filename ->
-           Caml.Format.printf "Found ml[i] file '%s' without cmt[i] file\n" filename
-         | Some source_filename, Some cmt_filename ->
-           let build_dir = "_build/default/" in
-           let wrap =
-             (* Format.printf "checking for prefix %S in %s\n%!" build_dir cmt_filename; *)
-             if String.is_prefix ~prefix:build_dir cmt_filename
-             then
-               if Caml.Sys.file_exists cmt_filename
-               then (fun f ->
-                 Unix.chdir build_dir;
-                 let infos =
-                   if Config.verbose ()
-                   then printfn "Reading cmt[i] file '%s'" cmt_filename;
-                   Cmt_format.read
-                     (String.drop_prefix cmt_filename (String.length build_dir))
-                 in
-                 f infos;
-                 Unix.chdir "../..")
-               else
-                 fun _ ->
-                 eprintf
-                   "File '%s' doesn't exist. Maybe some of source files are not compiled?\n"
-                   cmt_filename
-             else
-               fun f ->
-               let cmt = Cmt_format.read cmt_filename in
-               f cmt
-           in
-           wrap (on_cmti source_filename))
+      ~f:(function
+        | None, None -> ()
+        | Some filename, None ->
+          Caml.Format.printf "Found ml[i] file '%s' without cmt[i] file\n" filename
+        | None, Some filename ->
+          Caml.Format.printf "Found ml[i] file '%s' without cmt[i] file\n" filename
+        | Some source_filename, Some cmt_filename ->
+          let build_dir = "_build/default/" in
+          let wrap =
+            (* Format.printf "checking for prefix %S in %s\n%!" build_dir cmt_filename; *)
+            if String.is_prefix ~prefix:build_dir cmt_filename
+            then
+              if Caml.Sys.file_exists cmt_filename
+              then (fun f ->
+                Unix.chdir build_dir;
+                let infos =
+                  if Config.verbose ()
+                  then printfn "Reading cmt[i] file '%s'" cmt_filename;
+                  Cmt_format.read
+                    (String.drop_prefix cmt_filename (String.length build_dir))
+                in
+                f infos;
+                Unix.chdir "../..")
+              else
+                fun _ ->
+                eprintf
+                  "File '%s' doesn't exist. Maybe some of source files are not compiled?\n"
+                  cmt_filename
+            else
+              fun f ->
+              let cmt = Cmt_format.read cmt_filename in
+              f cmt
+          in
+          wrap (on_cmti source_filename))
   in
   let loop_database () =
     List.iter db ~f:(function
