2a015b4e09fc8996103f00f771c054015e947fb6
Kakadu <Kakadu@pm.me>
2024-07-25 17:39:59 +0300

    Simplice lint for license check
    Now we accept the code more often: we look colocated copyright and
    license information through all singature/structure items.
    We were checking the beginning of the file previously,
    but various PPX rewriters (bisect, expect, inline_test)
    could compilcate things
    
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- src/untyped/License.ml
+++ src/untyped/License.ml
@@ -1,3 +1,6 @@
+(** A check that every file starts from SPDX copyright and license information.
+    N.B. ppx could add extra attributes that complicate this check a little bit *)
+
 [@@@ocaml.text "/*"]
 
 (** Copyright 2021-2024, Dmitrii Kosarev a.k.a. Kakadu *)
@@ -6,23 +9,17 @@
 
 [@@@ocaml.text "/*"]
 
-(*
-   Checking that every file starts from SPDX copyright and license information.
-   N.B. ppx could add extra attributes that complicate this check a little bit
+(* TODO: support double licensing (AND keyword) *)
 
-   TODO: support double licensing (AND keyword)
-*)
+(** NB: We can omit appearance of the license in the generated documentation.
+    We should write like this:
 
-(* TODO: We can omit appearance of the license in the generated documentation.
-   We should write like this:
+    [@@@ocaml.text "/*"]
+    (** Copyright ... *)
+    (** SPDX-License-Identifier: ... *)
+    [@@@ocaml.text "/*"] *)
 
-   [@@@ocaml.text "/*"]
-   (** Copyright ... *)
-   (** SPDX-License-Identifier: ... *)
-   [@@@ocaml.text "/*"]
-*)
-
-open Caml.Format
+open Stdlib.Format
 open Zanuda_core
 open Utils
 
@@ -131,7 +128,7 @@ let panic ~loc ~filename mode =
        report
          ~loc
          ~filename
-         "OCaml files should start from copyright and license information"
+         "OCaml files should start with copyright and license information"
      | No_second_line ->
        report
          ~loc
@@ -148,21 +145,9 @@ let panic ~loc ~filename mode =
          ~loc
          ~filename
          {|Second item in file should be a documentation comment with correct license information. For example:
-           (** SPDX-License-Identifier: LGPL-3.0-or-later *)|})
+           (** SPDX-License-Identifier: AGPL-3.0-only *)|})
 ;;
 
-(*
-   Typical ppx_bisect support code:
-   + [@@@ocaml.text "/*"]
-   + module Bisect_visit___good2___ml =
-   + struct
-   + ...
-   + end
-   + open Bisect_visit___good2___ml
-   + [@@@ocaml.text "/*"]
-
-   It could be changed in the new version of PPX-bisect
-*)
 let run info fallback =
   let open Typedtree in
   let extract_string h1 =
@@ -175,134 +160,53 @@ let run info fallback =
     let pm = tsig_docattr __ in
     parse pm h1.sig_loc h1 ~on_error:(fun _ -> None) (fun s -> Some s)
   in
-  let is_ocaml_ppx_context item =
-    let open Tast_pattern in
-    let pm = tstr_attribute (attribute (string "ocaml.ppx.context") drop) in
-    parse pm item.str_loc item ~on_error:(fun _ -> false) true
-  in
-  let is_ocaml_ppx_context_sig item =
-    let open Tast_pattern in
-    let pm = tsig_attribute (attribute (string "ocaml.ppx.context") drop) in
-    parse pm item.sig_loc item ~on_error:(fun _ -> false) true
-  in
-  let is_ppx_bisect_artifact item =
-    let open Tast_pattern in
-    let pm =
-      let pat_bisect_mod () =
-        of_func (fun _ loc x k ->
-          match x.Typedtree.str_desc with
-          | Tstr_module { mb_name = { txt = Some name }; _ }
-            when String.starts_with ~prefix:"Bisect_visit" name -> k
-          | _ -> fail loc "")
-      in
-      let pat_bisect_open () =
-        of_func (fun _ loc x k ->
-          match x.Typedtree.str_desc with
-          | Tstr_open { open_expr = { mod_desc = Tmod_ident (_, { txt = Lident lid }) } }
-            when String.starts_with ~prefix:"Bisect_visit" lid -> k
-          | _ -> fail loc "")
-      in
-      let pat_bisect_c_comment () =
-        tstr_attribute
-          (attribute
-             (string "ocaml.text")
-             (of_func (fun _ loc x k ->
-                match x with
-                | Parsetree.PStr
-                    [ { pstr_desc =
-                          Pstr_eval
-                            ( { pexp_desc = Pexp_constant (Pconst_string ("/*", _, None)) }
-                            , _ )
-                      }
-                    ] -> k
-                | _ -> fail loc "Can't parse right payload")))
-      in
-      pat_bisect_mod () ||| pat_bisect_open () ||| pat_bisect_c_comment ()
-    in
-    parse pm item.str_loc item ~on_error:(fun _ -> false) true
-  in
   let filename = info.Compile_common.source_file in
-  let loc = Location.in_file filename in
+  let iter_items extract get_loc items =
+    let on_last_line () =
+      let loc, loc2 =
+        match items with
+        | [] ->
+          let l = Location.in_file info.Compile_common.source_file in
+          l, l
+        | h :: [] ->
+          let l = get_loc h in
+          l, l
+        | l1 :: l2 :: _ -> get_loc l1, get_loc l2
+      in
+      panic ~loc ~filename Bad_copyright_info;
+      panic ~loc:loc2 ~filename No_second_line
+    in
+    let rec loop = function
+      | l1 :: l2 :: tl ->
+        (match extract l1 with
+         | None -> loop (l2 :: tl)
+         | Some s when verify_line1 s ->
+           (* Line 1 verified *)
+           (match extract l2 with
+            | None -> panic ~loc:(get_loc l2) ~filename No_second_line
+            | Some s when not (verify_line2 s) ->
+              panic ~loc:(get_loc l2) ~filename Bad_copyright_info
+            | _ -> () (* success *))
+         | Some s ->
+           (* We skip unsuited lines. We hope that these lines were added by PPX rewriters,
+              and actual license information is below *)
+           loop (l2 :: tl))
+      | l1 :: [] ->
+        (match extract l1 with
+         | Some s when verify_line1 s ->
+           (* Copyright is written, license doesn't *)
+           panic ~loc:(get_loc l1) ~filename No_license_at_all
+         | _ -> on_last_line ())
+      | [] -> on_last_line ()
+    in
+    loop items
+  in
   let open Tast_iterator in
   { fallback with
     signature =
-      (fun _ { sig_items = items } ->
-        let wrap item mode =
-          let verifier, bad_spec =
-            match mode with
-            | `First -> verify_line1, Bad_copyright_info
-            | `Second -> verify_line2, Bad_license_info
-          in
-          match extract_string_sig item with
-          | None ->
-            let loc = item.sig_loc in
-            let filename = loc.Location.loc_start.Lexing.pos_fname in
-            panic ~loc ~filename bad_spec
-          | Some s when not (verifier s) ->
-            let loc = item.sig_loc in
-            let filename = loc.Location.loc_start.Lexing.pos_fname in
-            panic ~loc ~filename bad_spec
-          | Some _ -> ()
-        in
-        let () =
-          let rec loop = function
-            | h :: tl when is_ocaml_ppx_context_sig h -> loop tl
-            | h :: _ -> wrap h `First
-            | [] -> panic ~loc ~filename No_license_at_all
-          in
-          loop items
-        in
-        let () =
-          let rec loop = function
-            | h :: tl when is_ocaml_ppx_context_sig h -> loop tl
-            | _ :: h :: _ -> wrap h `Second
-            | [ _ ] | [] -> panic ~loc ~filename No_license_at_all
-          in
-          loop items
-        in
-        ())
+      (fun _ { sig_items } ->
+        iter_items extract_string_sig (fun l -> l.sig_loc) sig_items)
   ; structure =
-      (fun _ { str_items = items } ->
-        let wrap item mode =
-          let verifier, bad_spec =
-            match mode with
-            | `First -> verify_line1, Bad_copyright_info
-            | `Second -> verify_line2, Bad_license_info
-          in
-          match extract_string item with
-          | None ->
-            let loc = item.str_loc in
-            let filename = loc.Location.loc_start.Lexing.pos_fname in
-            panic ~loc ~filename bad_spec
-          | Some s when not (verifier s) ->
-            let loc = item.str_loc in
-            let filename = loc.Location.loc_start.Lexing.pos_fname in
-            panic ~loc ~filename bad_spec
-          | Some _ -> ()
-        in
-        (* The analysis is complicated because various preprocessors can
-           install code in the beginning of the file
-           TODO(Kakadu). Maybe we should look for two documentation comments
-           colocated in the file, and not in the beginning of the file?
-        *)
-        let () =
-          let rec loop = function
-            | h :: tl when is_ocaml_ppx_context h -> loop tl
-            | h :: tl when is_ppx_bisect_artifact h -> loop tl
-            | h :: _ -> wrap h `First
-            | [] -> panic ~loc ~filename No_license_at_all
-          in
-          loop items
-        in
-        let () =
-          let rec loop = function
-            | h :: tl when is_ocaml_ppx_context h -> loop tl
-            | h :: tl when is_ppx_bisect_artifact h -> loop tl
-            | _ :: h :: _ -> wrap h `Second
-            | [ _ ] | [] -> panic ~loc ~filename No_license_at_all
-          in
-          loop items
-        in
-        ())
+      (fun _ { str_items } -> iter_items extract_string (fun l -> l.str_loc) str_items)
   }
 ;;
