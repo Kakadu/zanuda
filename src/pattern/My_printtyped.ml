40c4d023b81c22183decd00358be76c03c866bd2
Kakadu <Kakadu@pm.me>
2025-03-25 12:48:39 +0300

    5.3 support in myprinttyped
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- src/pattern/My_printtyped.ml
+++ src/pattern/My_printtyped.ml
@@ -34,20 +34,42 @@ let expr ppf e =
     }
 ;;
 
+[%%if ocaml_version < (5, 0, 0)]
+
+let make_Tstr_module mb_name presence ~loc me =
+  Typedtree.Tstr_module
+    { mb_id = None
+    ; mb_name
+    ; mb_expr = me
+    ; mb_attributes = []
+    ; mb_presence = Types.Mp_present
+    ; mb_loc = loc
+    }
+;;
+
+[%%else]
+
+let make_Tstr_module mb_name presence ~loc me =
+  Typedtree.Tstr_module
+    { mb_id = None
+    ; mb_uid = Shape.Uid.internal_not_actually_unique
+    ; mb_name
+    ; mb_expr = me
+    ; mb_attributes = []
+    ; mb_presence = presence
+    ; mb_loc = loc
+    }
+;;
+
+[%%endif]
+
 let me ppf me =
   let open Typedtree in
   implementation
     ppf
     { str_items =
         [ { str_desc =
-              Tstr_module
-                { mb_id = None
-                ; mb_name = Location.mknoloc None
-                ; mb_expr = me
-                ; mb_attributes = []
-                ; mb_presence = Types.Mp_present
-                ; mb_loc = me.mod_loc
-                }
+              make_Tstr_module (Location.mknoloc None) Types.Mp_present ~loc:me.mod_loc me
           ; str_loc = me.mod_loc
           ; str_env = me.mod_env
           }
@@ -56,6 +78,23 @@ let me ppf me =
     ; str_type = []
     }
 ;;
+
+[%%if ocaml_version < (5, 0, 0)]
+let dummy_vb vb_pat vb_expr =
+  { Typedtree.vb_pat
+  ; vb_loc = Location.none
+  ; vb_attributes = []
+  ; vb_expr
+  }
+[%%else]
+let dummy_vb vb_pat vb_expr =
+  { Typedtree.vb_pat
+  ; vb_loc = Location.none
+  ; vb_rec_kind = Dynamic
+  ; vb_attributes = []
+  ; vb_expr
+  }
+[%%endif]
 
 let pattern ppf pat =
   let open Typedtree in
@@ -74,12 +113,7 @@ let pattern ppf pat =
         [ { str_desc =
               Tstr_value
                 ( Nonrecursive
-                , [ { vb_pat = pat
-                    ; vb_loc = Location.none
-                    ; vb_attributes = []
-                    ; vb_expr = dummy_expr
-                    }
-                  ] )
+                , [ dummy_vb pat dummy_expr ])
           ; str_loc = pat.pat_loc
           ; str_env = pat.pat_env
           }
@@ -89,6 +123,13 @@ let pattern ppf pat =
     }
 ;;
 
+[%%if ocaml_version < (5, 0, 0)]
+let make_int_const ~loc n = Pconst_integer (string_of_int n, None)
+[%%else]
+let make_int_const ~loc n =
+  { Parsetree.pconst_desc = Pconst_integer (string_of_int n, None); pconst_loc = loc }
+[%%endif]
+
 let attrs ppf attrs =
-  Pprintast.expression ppf (Ast_helper.Exp.constant (Pconst_integer ("1", None)) ~attrs)
+  Pprintast.expression ppf (Ast_helper.Exp.constant (make_int_const ~loc:Location.none 1) ~attrs)
 ;;
