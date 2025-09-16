178664192c7beb73ca46847bd2f11ce22b543c8e
Kakadu <Kakadu@pm.me>
2025-09-16 16:25:45 +0300

    Promote tests
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- tests/typed/MatchBool.t/MatchBool.ml
+++ tests/typed/MatchBool.t/MatchBool.ml
@@ -1,10 +1,11 @@
-let bool_match1 a = match a with 
+let bool_match1 a = match a with
   | true -> 1
   | false -> 2
 
-
 let bool_match2 a = match a with
   | false -> (
-    Format.printf "meow";
+    Format.printf "meow %d" 42;
     1)
   | true -> 2
+
+let foo x = function true -> 1 | false -> 2+x
